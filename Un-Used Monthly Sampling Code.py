3c98904882661646c82a8d69dc54f477fd607ea8
Dan Noel <dan@6noels.com>
2022-06-26 15:20:09 -0500

    initial checkin

---
+++ Un-Used Monthly Sampling Code.py
@@ -0,0 +1,40 @@
+#Un-used code for monthly sampling
+# def SampleByMonth(month):
+#     if ( month == 0 ):
+#         d = data[data.variable == "Jan"]
+#         return d["Rainfall"].sample()
+#     if ( month == 1 ):
+#         d = data[data.variable == "Feb"]
+#         return d["Rainfall"].sample()
+#     if ( month == 2 ):
+#         d = data[data.variable == "Mar"]
+#         return d["Rainfall"].sample()
+#     if ( month == 3 ):
+#         d = data[data.variable == "Apr"]
+#         return d["Rainfall"].sample()
+#     if ( month == 4 ):
+#         d = data[data.variable == "May"]
+#         return d["Rainfall"].sample()
+#     if ( month == 5 ):
+#         d = data[data.variable == "Jun"]
+#         return d["Rainfall"].sample()
+#     if ( month == 6 ):
+#         d = data[data.variable == "Jul"]
+#         return d["Rainfall"].sample()
+#     if ( month == 7 ):
+#         d = data[data.variable == "Aug"]
+#         return d["Rainfall"].sample()
+#     if ( month == 8 ):
+#         d = data[data.variable == "Sep"]
+#         return d["Rainfall"].sample()
+#     if ( month == 9 ):
+#         d = data[data.variable == "Oct"]
+#         return d["Rainfall"].sample()
+#     if ( month == 10 ):
+#         d = data[data.variable == "Nov"]
+#         return d["Rainfall"].sample()
+#     if ( month == 11 ):
+#         d = data[data.variable == "Dec"]
+# #         return d["Rainfall"].sample()
+
+#     return 2.356    
